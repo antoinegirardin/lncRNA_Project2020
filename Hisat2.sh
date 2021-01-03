9810f3aecd61fc680bdaec9956d5e2f4ffcb6d35
antoinegirardin <73240443+antoinegirardin@users.noreply.github.com>
2021-01-03 16:25:25 +0100

    Add comments

--- Hisat2.sh
+++ Hisat2.sh
@@ -9,23 +9,30 @@
 #SBATCH --output=/data/users/agirardin/output/output_hisat2_%j.o
 #SBATCH --error=/data/users/agirardin/error/error_hisat2_%j.e
 
+## Load hisat and samtools modules
 module add UHTS/Aligner/hisat/2.2.1
 module add UHTS/Analysis/samtools/1.10;
 
+## Define working directories
 READS_DIR=/data/courses/rnaseq/lncRNAs/Project2/
 HOME_DIR=/data/users/agirardin
 mkdir -p $HOME_DIR/hisat2
 
 ## Get reference genome indexes from https://daehwankimlab.github.io/hisat2/download/#h-sapiens
-# wget https://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
-# tar -xzf grch38_genome.tar.gz
+## wget https://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
+## tar -xzf grch38_genome.tar.gz
+## Reference files are stored in $READS_DIR/reference-files
 
+## Move to reads directory and list files
 cd $READS_DIR/fastq
 FILES=(*.fastq.gz)
 
+## Move to results directory and load reference file
 cd $HOME_DIR/hisat2
 ln -s $READS_DIR/reference-files/grch38 .
 
+## Loop over each paired reads and launch hisat2 for read mapping with reference genome
+## Transform .sam file to .bam file with samtools
 for i in `seq 0 11`
 do hisat2 -x grch38/genome -1 $READS_DIR/fastq/${FILES[2*${i}]} -2 $READS_DIR/fastq/${FILES[2*${i}+1]} -S Replicate_${i}.sam
 samtools view -bS Replicate_${i}.sam > Replicate_${i}.bam
