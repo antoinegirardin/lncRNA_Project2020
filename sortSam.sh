cfc533e36b0e6146d8a58a57194f1044742fb2b6
antoinegirardin <73240443+antoinegirardin@users.noreply.github.com>
2021-01-03 16:29:39 +0100

    Add comments

--- sortSam.sh
+++ sortSam.sh
@@ -9,27 +9,23 @@
 #SBATCH --output=/data/users/agirardin/output/output_sortBam_%j.o
 #SBATCH --error=/data/users/agirardin/error/error_sortBam_%j.e
 
+## Load samtools module
 module add UHTS/Analysis/samtools/1.10;
 
+## Define working directory and move to results directory
 HOME_DIR=/data/users/agirardin
-
 cd $HOME_DIR/hisat2
 
+## Loop over Replicates .bam file to sort them
 for i in `seq 0 3`
 do
-    samtools sort -o Replicate_$(echo 3*${i} | bc).sorted.bam Replicate_$(echo 3*${i} | bc).bam
-    samtools sort -o Replicate_$(echo 3*${i}+1 | bc).sorted.bam Replicate_$(echo 3*${i}+1 | bc).bam
-    samtools sort -o Replicate_$(echo 3*${i}+2 | bc).sorted.bam Replicate_$(echo 3*${i}+2 | bc).bam
+    samtools sort -@ 4 -o Replicate_$(echo 3*${i} | bc).sorted.bam Replicate_$(echo 3*${i} | bc).bam
+    samtools sort -@ 4 -o Replicate_$(echo 3*${i}+1 | bc).sorted.bam Replicate_$(echo 3*${i}+1 | bc).bam
+    samtools sort -@ 4 -o Replicate_$(echo 3*${i}+2 | bc).sorted.bam Replicate_$(echo 3*${i}+2 | bc).bam
 done
 
-samtools merge Sample1.bam Replicate_0.sorted.bam Replicate_1.sorted.bam Replicate_2.sorted.bam
-samtools merge Sample2.bam Replicate_3.sorted.bam Replicate_4.sorted.bam Replicate_5.sorted.bam
-samtools merge Sample3.bam Replicate_6.sorted.bam Replicate_7.sorted.bam Replicate_8.sorted.bam
-samtools merge SampleP.bam Replicate_9.sorted.bam Replicate_10.sorted.bam Replicate_11.sorted.bam
-
-# samtools sort Sample0.bam -o Sample1.sorted.bam
-# samtools sort Sample1.bam -o Sample2.sorted.bam
-# samtools sort Sample2.bam -o Sample3.sorted.bam
-# samtools sort Sample3.bam -o SampleP.sorted.bam
-
-# samtools merge meta-assembly.bam SampleP.sorted.bam Sample1.sorted.bam Sample2.sorted.bam Sample3.sorted.bam
+## Merge each replicates from the same cell subtypes
+samtools merge -@ 4 Sample1.bam Replicate_0.sorted.bam Replicate_1.sorted.bam Replicate_2.sorted.bam # Holoclone
+samtools merge -@ 4 Sample2.bam Replicate_3.sorted.bam Replicate_4.sorted.bam Replicate_5.sorted.bam # Meroclone
+samtools merge -@ 4 Sample3.bam Replicate_6.sorted.bam Replicate_7.sorted.bam Replicate_8.sorted.bam # Paraclone
+samtools merge -@ 4 SampleP.bam Replicate_9.sorted.bam Replicate_10.sorted.bam Replicate_11.sorted.bam # Parental
