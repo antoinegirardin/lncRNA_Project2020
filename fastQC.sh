e05e0752203c492ffed267dc25e8bc4af7876e57
antoinegirardin <73240443+antoinegirardin@users.noreply.github.com>
2021-01-03 16:20:46 +0100

    Update fastQC.sh
    Add comments

--- fastQC.sh
+++ fastQC.sh
@@ -10,21 +10,26 @@
 #SBATCH --output=/data/users/agirardin/output/output_fastqc_%j.o
 #SBATCH --error=/data/users/agirardin/error/error_fastqc_%j.e
 
+## Load fastQC and multiQC modules
 module add UHTS/Quality_control/fastqc/0.11.7
 module add UHTS/Analysis/MultiQC/1.8;
 
+## Define working directories
 READS_DIR=/data/courses/rnaseq/lncRNAs/Project2
 HOME_DIR=/data/users/agirardin
 mkdir -p $HOME_DIR/fastQC
 
+## Move to reads directory and read files list
 cd $READS_DIR/fastq
-
 FILES=(*.fastq.gz)
 
+## Move to results directory
 cd $HOME_DIR/fastQC
 
+## Loop over files to analyse them one by one with fastQC
 for i in `seq 0 23`
 do fastqc -t 6 $READS_DIR/fastq/${FILES[${i}]} -o $HOME_DIR/fastQC
 done 
 
+## Use multiQC to wrap up all fastQC results
 multiqc .
